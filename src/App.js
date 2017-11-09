deac67bf3f3b4b52b33d3d4160b5c64394c116d9
Sydney Coco <chnsydney@gmail.com>
2017-11-09 12:10:11 +0100

    "last"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import logo from './logo.svg';
+import cerfa from './assets/img/cerfa-dentiste.png'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1 className={appTitle}>Welcome to Cerfa-Dentiste</h1>
+          <img src={cerfa} className="cerfa" alt="logo" />
       </div>
     );
   }
 }
 
 export default App;
+
+<MuiThemeProvider>
+  <RaisedButton label="Default" />
+</MuiThemeProvider>
+
+const appTitle = {
+  z-index: -1;
+  height: 100vh;
+};
